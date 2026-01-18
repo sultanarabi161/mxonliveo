3f881c35c4b8197e3cf5a8037a0698ca69f112d5
SULTAN ARABI <168210639+sultanarabi161@users.noreply.github.com>
2026-01-18 23:29:56 +0600

    Create SplashActivity.java

---
+++ app/src/main/java/com/sultanarabi161/mxonlive/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.sultanarabi161.mxonlive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Will go to MainActivity after 3 seconds (3000 ms)
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // So that when you go back, you don't get to the splash screen again.
+            }
+        }, 3000);
+    }
+}
