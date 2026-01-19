28837f809fd0ccffd98fa16634a2e9a55211a5a7
SULTAN ARABI <168210639+sultanarabi161@users.noreply.github.com>
2026-01-19 09:02:02 +0600

    Update MainActivity.java

--- app/src/main/java/com/sultanarabi161/mxonlive/MainActivity.java
+++ app/src/main/java/com/sultanarabi161/mxonlive/MainActivity.java
@@ -1,7 +1,11 @@
 package com.sultanarabi161.mxonlive;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.PictureInPictureParams;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Rational;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -26,6 +30,36 @@ public class MainActivity extends AppCompatActivity {
         
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    // 1. When the Home button is pressed or the app is minimized, PiP mode will be activated
+    @Override
+    protected void onUserLeaveHint() {
+        super.onUserLeaveHint();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // A small window will be created with a 16:9 aspect ratio
+            PictureInPictureParams params = new PictureInPictureParams.Builder()
+                    .setAspectRatio(new Rational(16, 9))
+                    .build();
+            enterPictureInPictureMode(params);
+        }
+    }
+
+    // 2. While in PiP mode, the video should not pause
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+        if (isInPictureInPictureMode) {
+            // Hide the Action Bar in PiP mode (though our theme does not have one)
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().hide();
+            }
+        } else {
+            // When returning to full screen, restore everything back to normal
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().show();
+            }
+        }
     }
 
     @Override
